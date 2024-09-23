685c48ea86534ccd6196ab692761aee87045d6c8
Ayush Pandey <pandeyayush956@gmail.com>
2024-09-23 21:15:24 +0530

    Basic installation of react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur react | Today I learned about the installation of react and vite applications, also about the shortcuts that can be used to create react applications. We deleted some useless files which were present in src files of basicreact  </h1>
+  );
+}
+
+export default App;
