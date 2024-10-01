452a91988049870db5fe94b34c5e195fef20e4e2
Ayush Pandey <pandeyayush956@gmail.com>
2024-10-01 18:42:08 +0530

    Tailwind CSS

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Card({username,}) {
+    console.log(username);
+
+    
+  return (
+    <div className="flex flex-col rounded-xl  p-4"
+    style={{
+      border: '0.88px solid',
+
+      backdropFilter: 'saturate(180%) blur(14px)',
+      background: ' #ffffff0d',
+    }}
+  >
+    <div>
+      <img
+        src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+        alt="nft-gif"
+        width="400"
+        height="400"
+        className="rounded-xl"
+      />
+    </div>
+    <div className="flex flex-col  rounded-b-xl py-4 ">
+      <div className="flex justify-between">
+        <h1 className="font-RubikBold ">{username}</h1>
+        <h1 className="font-bold font-RubikBold">Price</h1>
+      </div>
+      <div className="flex  justify-between font-mono">
+        <p>#345</p>
+        <p>0.01</p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Card
