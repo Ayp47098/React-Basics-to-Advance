f1d01ebeca48b4b72fed0e5433b1583d5f141c3d
Ayush Pandey <pandeyayush956@gmail.com>
2024-10-03 19:37:20 +0530

    props+tailwind css

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -11,7 +11,10 @@ function App() {
 
   const addValue = () => {
     // counter = counter + 1
-    setCounter(counter+1)
+    setCounter((prevCounter => prevCounter + 1))
+    setCounter((prevCounter=> counter+1))
+    setCounter((prevCounter=> prevCounter+1))
+    setCounter((prevCounter=> prevCounter+1))
     console.log("clicked",counter);
   }
 
---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+function App() {
+  const [color,setColor] = useState("olive")
+
+  return (
+    <>
+     <div className="w-full"
+    </>
+  )
+}
+
+export default App
