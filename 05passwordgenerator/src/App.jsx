671172465f57b6bec0e14c43a68c524e9b99c0c8
Ayush Pandey <pandeyayush956@gmail.com>
2024-10-10 00:09:38 +0530

    useffect,useref,usecallback

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react'
 
-import './App.css'
+
 
 function App() {
   const [count, setLength] = useState(8)
@@ -11,13 +11,35 @@ function App() {
   const passwordGenerator= useCallback(()=> {
     let pass=""
     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvxyz"
-    if (numberAllowed) str +="123456789"
-  }(fn,[length,numberAllowed,charAllowed,setPassword])
-  const passwordGenerator= () => {}
+    if (numberAllowed) str +="0123456789"
+    if (charAllowed) str +="!@#$%^&*-_+=[]{}~`"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char= Math.floor(Math.random()* str.length + 1)
+      pass = str.charAt(char)
+
+    }
+
+    setPassword(pass)
+
+  },[length,numberAllowed,charAllowed,setPassword])
+ 
   return (
     <>
    
-     <h1 className='text-4xl text-center text-white '>Password Generator</h1>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center'>Password generator></h1>
+      <div 'className="flex shadow rounded-lg overflow-hidden mb-4"'>
+      <input 
+        type='text'
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='password'
+        readOnly
+
+      />
+      </div> 
+    </div>
     </>
   )
 }
